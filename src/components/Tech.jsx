e8e890bb9a84eb237b5f6f525621feba0ff0a439
Henry Tito <140865436+HenryTito@users.noreply.github.com>
2026-02-09 21:51:24 -0300

    feat(Tech):add section head text for tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,35 +1,27 @@
 import React from 'react'
-import {BallCanvas} from "./canvas"
+import { BallCanvas } from "./canvas"
 import SectionWrapper from '../hoc'
 import { technologies } from '../constants'
-import { div } from 'framer-motion/client'
 import { styles } from '../styles'
 
 const Tech = () => {
   return (
-
-    
-
-
-    <div className='flex flex-row flex-wrap
-    justify-center gap-10
-    
-    
-    '>
+    <div className="flex flex-col items-center">
       
       
-    {technologies.map((technology)=> (
-        <div className='w-28 h-28' key={technology.name}>
+      <h2 className={`${styles.sectionHeadText} text-center mb-10`}>
+        Tecnologias
+      </h2>
+      <h3 className={`${styles.sectionSubText} text-center mb-10`}>(rotacione as esferas!)</h3>
 
-            
+      
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
             <BallCanvas icon={technology.icon} />
-
-        </div>
-
-
-    ))}
-
-
+          </div>
+        ))}
+      </div>
 
     </div>
   )
