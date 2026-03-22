63719b3185b15a523c9b2e669992a4749aef2b56
Henry Tito <140865436+HenryTito@users.noreply.github.com>
2026-03-22 17:44:48 -0300

    feat: add new resume for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
             </p>
 
             <div className="flex justify-center lg:justify-start">
-              <a href="/files/CurriculoHenryTitoOficial.pdf" download>
+              <a href="/files/CURRICULOHENRYOFICIAL.pdf" download>
                 <button
                   className="
                     mt-6
